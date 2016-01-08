8de449550ca403c06d213d8586a8cd8af0bed345
jfourmond <jerome@fourmond.fr>
2016-01-08 11:44:15 +0100

    Interface avec instances et règles

--- AI-FOIL/src/Interface.java
+++ AI-FOIL/src/Interface.java
@@ -2,13 +2,20 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Vector;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 
 import weka.core.Attribute;
 import weka.core.Instance;
@@ -18,10 +25,20 @@ public class Interface extends JFrame implements ActionListener{
 	
 	private static final long serialVersionUID = 1L;
 	
+	private JMenuBar menu_bar;
+	private JMenu file;
+		private JMenuItem item_load;
+		private JMenuItem item_exit;
 	private JPanel main_panel;
-		private JTable table_instances;
-		private JScrollPane scroll_panel;
-		
+		private JPanel center_panel;
+			private JTable table_instances;
+			private JScrollPane scroll_panel;
+		private JPanel down_panel;
+			private JTextArea rules_area;
+			private JScrollPane scroll_panel_text;
+	
+	private JFileChooser fc;
+			
 	private String filename;
 	private Instances instances;
 	
@@ -45,46 +62,95 @@ public class Interface extends JFrame implements ActionListener{
 	}
 
 	private void buildComposants() throws Exception {
+		fc = new JFileChooser(new File("~"));
+		
+		menu_bar = new JMenuBar();
+		file = new JMenu("Fichier");
+			item_load = new JMenuItem("Charger");
+			item_exit = new JMenuItem("Quitter");
+		
 		main_panel = new JPanel(new BorderLayout());
-			// Création de l'en-tête
-			Vector<String> en_tete = new Vector<>(instances.numAttributes());
-			for(int i=0 ; i<instances.numAttributes() ; i++) {
-				Attribute attribute = instances.attribute(i);
-				en_tete.add(attribute.name());
-			}
-			System.out.println(en_tete);
-			// Création des valeurs
-			Vector<Vector <String>> rowData = new Vector<>(instances.numInstances());
-			for(int i=0 ; i<instances.numInstances() ; i++) {
-				Vector<String> V = new Vector<>();
-				rowData.add(V);
-			}
-			for(int i=0 ; i<instances.numInstances() ; i++) {
-				Instance instance = instances.instance(i);
-				for(int j=0 ; j<instances.numAttributes() ; j++) {
-					Attribute attribute = instance.attribute(j);
-					rowData.elementAt(i).add(instance.stringValue(attribute));
+			center_panel = new JPanel();
+				// Création de l'en-tête
+				Vector<String> en_tete = new Vector<>(instances.numAttributes());
+				for(int i=0 ; i<instances.numAttributes() ; i++) {
+					Attribute attribute = instances.attribute(i);
+					en_tete.add(attribute.name());
 				}
-			}
-			table_instances = new JTable(rowData, en_tete);
-			scroll_panel = new JScrollPane(table_instances);
+				System.out.println(en_tete);
+				// Création des valeurs
+				Vector<Vector <String>> rowData = new Vector<>(instances.numInstances());
+				for(int i=0 ; i<instances.numInstances() ; i++) {
+					Vector<String> V = new Vector<>();
+					rowData.add(V);
+				}
+				for(int i=0 ; i<instances.numInstances() ; i++) {
+					Instance instance = instances.instance(i);
+					for(int j=0 ; j<instances.numAttributes() ; j++) {
+						Attribute attribute = instance.attribute(j);
+						rowData.elementAt(i).add(instance.stringValue(attribute));
+					}
+				}
+				table_instances = new JTable(rowData, en_tete);
+				table_instances.setEnabled(false);
+				table_instances.setAutoCreateRowSorter(true);
+				scroll_panel = new JScrollPane(table_instances);
+			down_panel = new JPanel();
+				rules_area = new JTextArea();
+				// Calcul des règles et affichage
+				// TODO effectuer le calcul dans un thread pour ne pas gêner l'affichage
+				ArrayList<Rule> rules = Main.couvertureSequentielle(instances);
+				for(Rule R : rules) {
+					rules_area.append(R.toString() + "\n");
+				}
+				rules_area.setEditable(false);
+				scroll_panel_text = new JScrollPane(rules_area);
 	}
 	
 	private void buildInterface() {
-		main_panel.add(scroll_panel, BorderLayout.CENTER);
-		 this.setContentPane(main_panel);
-		//this.setContentPane(scroll_panel);
+		file.add(item_load);
+		file.add(item_exit);
+		menu_bar.add(file);
+		
+			center_panel.add(scroll_panel);
+			down_panel.add(scroll_panel_text);
+		main_panel.add(center_panel, BorderLayout.CENTER);
+		main_panel.add(down_panel, BorderLayout.SOUTH);
+		
+		setJMenuBar(menu_bar);
+		setContentPane(main_panel);
 	}
 	
 	private void buildEvents() {
-		// TODO Auto-generated method stub
+		item_exit.addActionListener(this);
+		item_load.addActionListener(this);
+		
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		Object O = e.getSource();
+		if(O.getClass() == JMenuItem.class) {
+			JMenuItem MI = (JMenuItem) O;
+			if(MI == item_exit)
+				System.exit(EXIT_ON_CLOSE);
+			else if(MI == item_load) {
+				File fichier;
+				if (fc.showOpenDialog(null)== 
+				    JFileChooser.APPROVE_OPTION) {
+				    fichier = fc.getSelectedFile();
+				    System.out.println(fichier.getAbsolutePath());
+				    try {
+						new Interface(fichier.getAbsolutePath());
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		}
 	}
 
 }
