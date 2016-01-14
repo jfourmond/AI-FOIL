864b6a0c991b887de94ce571864e396c5d03e390
jdefaye <johan.defaye@hotmail.fr>
2016-01-14 09:39:41 +0100

    Finalisation de l

--- AI-FOIL/src/PrintConsole.java
+++ AI-FOIL/src/PrintConsole.java
@@ -14,13 +14,23 @@ public class PrintConsole {
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
 	
+	public static void print_space(int nb_space) {
+		for (int i = 0; i < nb_space; ++i) {
+			System.out.print(" ");
+		}
+	}
+	
 	public static void data(Instances instances) {
 		// Affichage des headers
 		System.out.print("E\t|    ");
+		int size_row = 10;
 		for(int i=0 ; i<instances.numAttributes() ; i++) {
 			Attribute attribute = instances.attribute(i);
 			System.out.print(attribute.name() + "    |    ");
+			size_row += 9 + attribute.name().length();
 		}
+		System.out.println();
+		for (int i = 0; i < size_row/2; ++i) System.out.print("- ");
 		System.out.println();
 		for(int i=0 ; i<instances.numInstances() ; i++) {
 			Instance instance = instances.instance(i);
@@ -28,31 +38,18 @@ public class PrintConsole {
 			for(int j=0 ; j<instances.numAttributes() ; j++) {
 				Attribute attribute = instance.attribute(j);
 				int diff_longueur = instance.stringValue(attribute).length()-attribute.name().length();
-				switch (diff_longueur) {
-					case -15: System.out.print("           "+instance.stringValue(attribute)+"            |"); break;
-					case -14: System.out.print("           "+instance.stringValue(attribute)+"           |"); break;
-					case -13: System.out.print("          "+instance.stringValue(attribute)+"           |"); break;
-					case -12: System.out.print("          "+instance.stringValue(attribute)+"          |"); break;
-					case -11: System.out.print("         "+instance.stringValue(attribute)+"          |"); break;
-					case -10: System.out.print("         "+instance.stringValue(attribute)+"         |"); break;
-					case -9 : System.out.print("        "+instance.stringValue(attribute)+"         |"); break;
-					case -8 : System.out.print("        "+instance.stringValue(attribute)+"        |"); break;
-					case -7 : System.out.print("       "+instance.stringValue(attribute)+"        |"); break;
-					case -6 : System.out.print("       "+instance.stringValue(attribute)+"       |"); break;
-					case -5 : System.out.print("      "+instance.stringValue(attribute)+"       |"); break;
-					case -4 : System.out.print("      "+instance.stringValue(attribute)+"      |"); break;
-					case -3 : System.out.print("     "+instance.stringValue(attribute)+"      |"); break;
-					case -2 : System.out.print("     "+instance.stringValue(attribute)+"     |"); break;
-					case -1 : System.out.print("    "+instance.stringValue(attribute)+"     |"); break;
-					case 0  : System.out.print("    "+instance.stringValue(attribute)+"    |"); break;
-					case 1  : System.out.print("    "+instance.stringValue(attribute)+"   |"); break;
-					case 2  : System.out.print("   "+instance.stringValue(attribute)+"   |"); break;
-					case 3  : System.out.print("   "+instance.stringValue(attribute)+"  |"); break;
-					case 4  : System.out.print("  "+instance.stringValue(attribute)+"  |"); break;
-					case 5  : System.out.print("  "+instance.stringValue(attribute)+" |"); break;
-					case 6  : System.out.print(" "+instance.stringValue(attribute)+" |"); break;
-					default : System.out.print(" "+instance.stringValue(attribute).substring(0, 5+attribute.name().length())+" |"); break;
-				}
+					if ((8-diff_longueur) % 2 == 0) {
+						print_space((8-diff_longueur)/2);
+						System.out.print(instance.stringValue(attribute));
+						print_space((8-diff_longueur)/2);
+						System.out.print("|");
+					}
+					else {
+						print_space(1+((8-diff_longueur)/2));
+						System.out.print(instance.stringValue(attribute));
+						print_space((8-diff_longueur)/2);
+						System.out.print("|");
+					}
 			}
 			System.out.println();
 		}
